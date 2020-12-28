31cc7f68da134f9c79260cd69d53ece0c9726a01
MD. Mohiuddin ahmed efti <74891305+MohiuddinEfti@users.noreply.github.com>
2020-12-28 20:53:34 +0600

    Update AddEmployee.cs

--- Restuarent/AddEmployee.cs
+++ Restuarent/AddEmployee.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
@@ -26,66 +26,7 @@ namespace Restuarent
 
         private void AddEmployeeButton_Click(object sender, EventArgs e)
         {
-            string gen = null;
-            if (radioButton1.Checked)
-            {
-                gen = "Female";
-            }
-            else
-            {
-                gen = "Male";
-            }
-
-            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Employees"].ConnectionString);
-            connection.Open();
-            string sql = "INSERT INTO Employees(Name,Email,PhoneNo,DateOfBirth,Gender,BloodGroup) VALUES('" + NameTextBox.Text + "','" + EmailTextBox.Text + "','" + Phonetextbox.Text + "','" + dateTimePicker1.Text + "','" + gen + "','" + comboBox1.Text + "')";
-            SqlCommand command = new SqlCommand(sql, connection);
-            int result = command.ExecuteNonQuery();
-            connection.Close();
-            if (result > 0)
-            {
-
-                {
-                    if (NameTextBox.Text == "")
-                    {
-
-                        MessageBox.Show("You have not given any name in the provided space");
-                    }
-
-                    else if (Phonetextbox.Text == "")
-                    {
-
-                        MessageBox.Show("You have not given the employees phone number in the provided space");
-                    }
-                    else if (EmailTextBox.Text == "")
-                    {
-                        MessageBox.Show("You have not given employees email in the provided space");
-                    }
-                    else if (radioButton1.Checked == false && radioButton2.Checked == false)
-                    {
-                        MessageBox.Show("Error!!\nThe password you have typed does not match with the password you have confirmed.\nPlease type again.");
-                    }
-
-                    else
-                    {
-                        MessageBox.Show("All employee details have been successfully added.");
-                        Manager M = new Manager();
-                        M.Show();
-                        this.Hide();
-
-
-                    }
-                }
-
-            }
-
-            else
-            {
-                MessageBox.Show("Error!!!\nYAdding of employee details was unsuccessful.\nPlease try again.");
-                Manager M = new Manager();
-                M.Show();
-                this.Hide();
-            }
+            
 
         }
     }
