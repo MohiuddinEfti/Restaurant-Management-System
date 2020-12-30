5e94eaff7ccf9b821bdbbee63aed1f83b0e62713
MD. Mohiuddin ahmed efti <ahmedefti277@gmail.com>
2020-12-30 20:31:05 +0600

    Update

--- Restuarent/Chef.Designer.cs
+++ Restuarent/Chef.Designer.cs
@@ -36,6 +36,8 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(33, 12);
             this.dataGridView1.Name = "dataGridView1";
